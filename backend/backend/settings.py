8e0e213433d7fa08db3623983b6beb77e24f0853
Sam_George <salty_bolls>
2025-07-21 03:24:16 +0100

    reset allowed hosts to all temporarily

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["foodsboox-coming-soon-page.onrender.com"]
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
